7145b2da7a35b14143dff44574aaa11e721c4625
Jason Sakamoto <jasons03@uw.edu>
2024-05-02 19:46:03 -0700

    Implemeted Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Navbar from "./Navbar.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Test from './Test.js'; 
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-    </div>
+      <div className="app">
+        <Router>
+        <Navbar />
+        <div>
+            <Routes>
+              <Route path="/Test" element={ <Test />} />
+            </Routes>
+        </div>
+        </Router>
+      </div>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
   return (
     <>
@@ -8,9 +10,7 @@ function Navbar() {
             <div className="text-3xl text-white">Cafetal Quilombo Cafe</div>
           </div>
           <div className="px-[40px] py-[20px] flex items-center space-x-10">
-            <div className="text-white">About Us</div>
-            <div className="text-white">Menu</div>
-            <div className="text-white">Catering</div>
+            <Link to="/Test" className="text-white">Test</Link>
           </div>
         </div>
       </div>
