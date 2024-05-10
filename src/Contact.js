31b59d23a8785f9ac220ef58c4e492963d08c7c1
Jason Sakamoto <jasons03@uw.edu>
2024-05-09 18:14:55 -0700

    Text formatting

--- src/Contact.js
+++ src/Contact.js
@@ -6,10 +6,10 @@ function Contact() {
     <>
       <div className="mx-auto font-serif">
         <div className="h-[235px] w-full bg-[url('./Images/contactImage.png')] bg-center bg-cover border-b-[6px] border-burgundy">
-          <div className="mx-auto flex justify-center border-8 border-burgundy rounded-[16px] w-[980px] h-[146px] translate-y-[160px] bg-white">
-            <div className="flex-col">
-            <h1 className="text-[24px] py-2 font-bold">Catering</h1>
-            <p className="px-[48px] text-[18px] leading-5">
+          <div className="mx-auto flex-col justify-center border-8 border-burgundy rounded-[16px] w-[980px] h-[146px] translate-y-[160px] bg-white">
+            <div className="">
+            <h1 className="text-[24px] py-2 font-bold text-center">Catering</h1>
+            <p className="px-[48px] text-[18px] leading-5 text-center">
               Looking to serve a large group? We can cater large orders of your
               favorite dishes, complete with delicious sides. Our typical
               requests are for tamales, tacos, or enchiladas, but if there's
