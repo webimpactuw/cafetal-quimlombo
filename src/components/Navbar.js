7eb35cf258a797388331eb08f7fad62cb2791449
RedPandaGaminq <timothyzhong2@gmail.com>
2024-05-24 14:57:01 -0700

    Adapted menu page to all screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import cafeLogo from "../images/logo.png";
 
 function Navbar() {
   return (
-    <div className="fixed z-50 w-screen flex justify-between bg-red-primary text-xl text-white font-semibold">
+    <div className="hidden fixed z-50 w-screen md:flex justify-between bg-red-primary text-xl text-white font-semibold">
       {/* LOGO */}
       <div className="px-[40px] py-[20px] flex gap-6 items-center">
         <Link to="/" className="w-[42px] h-[42px] object-cover">
