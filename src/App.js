35539cb6aeff8c1d86bbc6c9959cee42edb71744
Timothy <timothyzhong2@gmail.com>
2024-06-10 16:33:07 -0700

    Fixed links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./scrollToTop.js";
-import Navbar from "./Navbar.js";
-import MobileNavbar from './MobileNavBar.js';
+import Navbar from "./components/Navbar.js";
+import MobileNavbar from "./components/Navbar--Mobile.js";
 import Footer from "./components/Footer.js";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
@@ -13,8 +13,9 @@ function App() {
   return (
     <div className="App text-gray-primary">
       <Router>
-        <ScrollToTop/>
-        <MobileNavbar />
+        <ScrollToTop />
+        <Navbar />
+        {/* <MobileNavbar /> */}
         <div>
           <Routes>
             <Route path="/" element={<Home />}></Route>
--- src/components/Navbar--Mobile.js
+++ src/components/Navbar--Mobile.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
-import cafeLogo from "./Logo.png";
+import cafeLogo from "../images/logo.png";
 
 function MobileNavbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,15 +14,40 @@ function MobileNavbar() {
       <div className="bg-red text-xl w-screen fixed z-50">
         <div className="flex justify-between">
           <div className=" pl-3 py-[20px] flex space-x-4 items-center">
-            <Link to="/" className="object-cover w-[42px] h-[42px]" onClick={(isOpen ? toggleMenu : "")}>
+            <Link
+              to="/"
+              className="object-cover w-[42px] h-[42px]"
+              onClick={isOpen ? toggleMenu : ""}
+            >
               <img src={cafeLogo} alt="logo"></img>
             </Link>
-            <Link to="/" className="text-2xl text-white pr-4" onClick={(isOpen ? toggleMenu : "")}>Cafetal Quilombo Cafe</Link>
+            <Link
+              to="/"
+              className="text-2xl text-white pr-4"
+              onClick={isOpen ? toggleMenu : ""}
+            >
+              Cafetal Quilombo Cafe
+            </Link>
           </div>
-          <div
-            className="pr-[20px] py-[20px]"
-            onClick={toggleMenu}
-          ><svg  xmlns="http://www.w3.org/2000/svg"  width="48"  height="48"  viewBox="0 0 24 24"  fill="none"  stroke="#ffffff"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg></div>
+          <div className="pr-[20px] py-[20px]" onClick={toggleMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 6l16 0" />
+              <path d="M4 12l16 0" />
+              <path d="M4 18l16 0" />
+            </svg>
+          </div>
         </div>
         <div
           className={
